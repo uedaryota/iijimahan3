3e0faa83bd06b3147ddd9a58db57ba1bf18ff852
sugahararyota <s.ryota072@gmail.com>
2021-03-05 03:19:59 +0900

    マテリアル、プレハブ、スクリプトファイルを追加。　 Playerの移動、マウスの方向に弾を飛ばすまで完成

---
+++ iijimahan/Assets/Script/BulletControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    private Vector3 velocity;
+    public float speed = 20.0f;
+    private int timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 600) Destroy(this.gameObject);
+        velocity.Normalize();
+        velocity *= speed;
+        transform.position += velocity * Time.deltaTime;
+        timer++;
+    }
+    public void SetVelocity(Vector3 vec3)
+    {
+        velocity = vec3;
+    }
+    public void SetPosition(Vector3 pos)
+    {
+        transform.position = pos;
+    }
+    public void SetTransform(Vector3 vec3, Vector3 pos)
+    {
+        velocity = vec3;
+        transform.position = pos;
+    }
+}
---
+++ iijimahan/Assets/Script/PlayerControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject bullet;
+    private Vector3 velocity;
+    public float speed =10f;
+    public int HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        transform.position += velocity * Time.deltaTime;
+        velocity = Vector3.zero;
+        Vector3 screen_point = Input.mousePosition;
+        Vector3 screen_playerPos = RectTransformUtility.WorldToScreenPoint(Camera.main, this.transform.position);
+        //Debug.Log(screen_point);
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 弾丸の複製
+            GameObject bullets = Instantiate(bullet) as GameObject;
+            // クリックした座標の取得（スクリーン座標からワールド座標に変換）
+            // Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // 向きの生成（Z成分の除去と正規化）
+            //Vector3 vel = Vector3.Scale((mouseWorldPos - transform.position), new Vector3(1, 1, 0)).normalized;
+            Vector3 a = screen_point - screen_playerPos;
+            a.z = 0;
+            bullets.GetComponent<BulletControl>().SetTransform(a, this.transform.position);
+        }
+        // z キーが押された時
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            // 弾丸の複製
+            GameObject bullets = Instantiate(bullet) as GameObject;
+            // クリックした座標の取得（スクリーン座標からワールド座標に変換）
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // 向きの生成（Z成分の除去と正規化）
+            Vector3 vel = Vector3.Scale((mouseWorldPos - transform.position), new Vector3(1, 1, 0)).normalized;
+            //Vector3 vel;
+            //vel = new Vector3(0.2f, 0, 0);
+            bullets.GetComponent<BulletControl>().SetTransform(vel, this.transform.position);
+        }
+    }
+    public void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += new Vector3(0, speed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity += new Vector3(0, -speed, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity += new Vector3(-speed, 0, 0);
+        }
+        velocity.Normalize();
+        velocity *= speed;
+    }
+}
