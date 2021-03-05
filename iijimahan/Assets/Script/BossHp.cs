64d0c8f28193bfbf8e823a258781dedf7852fa24
iori_useful <alcor3683@gmail.com>
2021-03-05 13:52:33 +0900

    ボスの体力スクリプト(仮実装) 無敵時間の設定可能 裏返しエネミーの弾のタグが出来次第反映させる必要あり

--- iijimahan/Assets/Script/BossHp.cs
+++ iijimahan/Assets/Script/BossHp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BossHp : MonoBehaviour
 {
     #region//インスペクターで設定する
@@ -15,12 +16,20 @@ public class BossHp : MonoBehaviour
 
     public Status status;
     public float Hp;//ボス現在体力
+    float damege = 0;
+    float alpha_Sin=255;
+    bool SetRev = false;
+    Color _color;
 
     // Start is called before the first frame update
     void Start()
     {
         status = Status.Normal;
         Hp = MaxHp;
+        damege = 0;
+        alpha_Sin = 255;
+        _color = this.gameObject.GetComponent<Renderer>().material.color;
+        SetRev = false;
     }
 
     // Update is called once per frame
@@ -30,10 +39,50 @@ public class BossHp : MonoBehaviour
         {
             Dead();
         }
+        if(status==Status.Damege)
+        {
+            if (!SetRev)
+            {
+                alpha_Sin--;
+                if(alpha_Sin<1)
+                {
+                    SetRev = true;
+                }
+            }
+            else
+            {
+                alpha_Sin++;
+                if(alpha_Sin>254)
+                {
+                    SetRev = false;
+                }
+            }
+            Invoke("ArmorOff",Superarmor);
+            Col();
+        }
     }
 
     void Dead()
     {
         Destroy(gameObject);
     }
+    public void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag=="EnemyBullet" &&status==0)//味方になったエネミーの弾のタグを設定
+        {
+            //damege = other.gameObject.GetComponent<>();
+            Hp = Hp - damege;
+            status = Status.Damege;
+        }
+    }
+    public void ArmorOff()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = _color;
+        status = Status.Normal;
+    }
+   public void Col()
+   { 
+        //_color.a = alpha_Sin;
+        this.gameObject.GetComponent<Renderer>().material.color = Color.black;//_color;     
+   }
 }
