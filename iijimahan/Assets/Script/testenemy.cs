3c874d4ff10e0c74a212f6d72cab84ece183d047
uedaryota <mr.335186@gmail.com>
2021-03-05 12:03:19 +0900

    エネミーマネージャー追加

---
+++ iijimahan/Assets/Script/testenemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testenemy : MonoBehaviour
+{
+    private Vector3 velocity;
+    public float speed = 0.1f;
+    
+    void Update()
+    {
+        velocity -= new Vector3(speed, 0, 0);
+        transform.position += velocity * Time.deltaTime;
+
+        if(velocity.x <= -40.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
