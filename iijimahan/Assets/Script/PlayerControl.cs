eb86fc82ce7f2976781750bf888326114ccea0b2
sugahararyota <s.ryota072@gmail.com>
2021-03-05 10:48:01 +0900

    プレイヤーのstateとHP処理追加

--- iijimahan/Assets/Script/PlayerControl.cs
+++ iijimahan/Assets/Script/PlayerControl.cs
@@ -6,30 +6,57 @@ public class PlayerControl : MonoBehaviour
 {
     public GameObject bullet;
     private Vector3 velocity;
-    public float speed =10f;
-    public int HP;
+    public float speed = 10f;
+    public int HP = 100;
+    public int gauge = 0;
+
+    public enum PlayerState
+    {
+        Alive,
+        Dead,
+    }
+
+    private PlayerState playerState;
+    public PlayerState GetPlayerState { get => playerState; set => playerState = value; }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerState = PlayerState.Alive;
     }
     // Update is called once per frame
     void Update()
     {
+        //デバッグ用*******************************
+        gauge++;
+        if (gauge > 100) gauge = 100;//Debug.Log(gauge);
+        //デバッグ用*******************************
+        if( HP <= 0 )
+        {
+            playerState = PlayerState.Dead;
+        }
+
         Move();
         transform.position += velocity * Time.deltaTime;
         velocity = Vector3.zero;
+
         Vector3 screen_point = Input.mousePosition;
         Vector3 screen_playerPos = RectTransformUtility.WorldToScreenPoint(Camera.main, this.transform.position);
-        //Debug.Log(screen_point);
+        
+        //マウスの右クリックで弾発射
         if (Input.GetMouseButtonDown(0))
         {
-            // 弾丸の複製
             GameObject bullets = Instantiate(bullet) as GameObject;
-            Vector3 a = screen_point - screen_playerPos;
-            a.z = 0;
-            bullets.GetComponent<BulletControl>().SetTransform(a, this.transform.position);
+            Vector3 vel = screen_point - screen_playerPos;
+            vel.z = 0;
+            bullets.GetComponent<BulletControl>().SetTransform(vel, this.transform.position);
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("敵をひっくり返す技");
+            gauge -= 40;
         }
         
     }
