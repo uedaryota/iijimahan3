50a8147ae3f2e6d0a36eafac354ded2f35811465
sugahararyota <s.ryota072@gmail.com>
2021-03-05 09:38:52 +0900

    プレイヤーの色緑に変更

--- iijimahan/Assets/Script/PlayerControl.cs
+++ iijimahan/Assets/Script/PlayerControl.cs
@@ -8,6 +8,7 @@ public class PlayerControl : MonoBehaviour
     private Vector3 velocity;
     public float speed =10f;
     public int HP;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,27 +27,11 @@ public class PlayerControl : MonoBehaviour
         {
             // 弾丸の複製
             GameObject bullets = Instantiate(bullet) as GameObject;
-            // クリックした座標の取得（スクリーン座標からワールド座標に変換）
-            // Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            // 向きの生成（Z成分の除去と正規化）
-            //Vector3 vel = Vector3.Scale((mouseWorldPos - transform.position), new Vector3(1, 1, 0)).normalized;
             Vector3 a = screen_point - screen_playerPos;
             a.z = 0;
             bullets.GetComponent<BulletControl>().SetTransform(a, this.transform.position);
         }
-        // z キーが押された時
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            // 弾丸の複製
-            GameObject bullets = Instantiate(bullet) as GameObject;
-            // クリックした座標の取得（スクリーン座標からワールド座標に変換）
-            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            // 向きの生成（Z成分の除去と正規化）
-            Vector3 vel = Vector3.Scale((mouseWorldPos - transform.position), new Vector3(1, 1, 0)).normalized;
-            //Vector3 vel;
-            //vel = new Vector3(0.2f, 0, 0);
-            bullets.GetComponent<BulletControl>().SetTransform(vel, this.transform.position);
-        }
+        
     }
     public void Move()
     {
