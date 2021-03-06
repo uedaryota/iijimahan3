2a1ced657b72bbfaf64889102e46f8cb0a61a8a4
uedaryota <mr.335186@gmail.com>
2021-03-06 17:49:28 +0900

    ボスの生成

--- iijimahan/Assets/Script/EnemyManager.cs
+++ iijimahan/Assets/Script/EnemyManager.cs
@@ -5,38 +5,96 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     [SerializeField, Header("enemyboxオブジェクト")] List<Object> enemyboxes;
-    [SerializeField, Header("生成間隔")] private float interval;
+    [SerializeField, Header("enemyboxオブジェクトのそれぞれのエネミー数")] List<int> enemynum;
+    [SerializeField, Header("bossオブジェクト")] Object boss;
+    [SerializeField, Header("生成間隔")] float interval = 3;
     [SerializeField, Header("enemyのランダム生成用のy座標(高)")] float y_high = 0;
     [SerializeField, Header("enemyのランダム生成用のy座標(低)")] float y_low = 0;
     [SerializeField, Header("enemyのランダム生成用のx座標(高)")] float x_high = 0;
     [SerializeField, Header("enemyのランダム生成用のx座標(低)")] float x_low = 0;
+    [SerializeField, Header("enemyの生成上限")] int enemycountlimit = 10;
+    [SerializeField, Header("wave")] int wave = 1;
+    
     private float timer;
     private float x_rnd;
     private float y_rnd;
+    private int old_wave;
+
     private int enemyrnd;
+    private int enemycount;//エネミー生成用のカウント
+    private int enemydeathcount = 0;//エネミーの倒れた数のカウント
     
     void Update()
     {
+        old_wave = wave;
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            wave = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            wave = 2;
+        }
+        switch (wave)
+        {
+            case 1:
+                enemyrnd = Random.Range(0, 1);
+                EnemyRespawn();
+                return;
+
+            case 2:
+                enemyrnd = Random.Range(0, 1);
+                BossRespawn();
+                EnemyRespawn();
+                return;
+        }
+    }
+
+    void EnemyRespawn()
+    {
         timer += Time.deltaTime;
         
-        if(timer > interval)
+        if (enemycountlimit >= enemycount)
         {
-            timer = 0;
+            if (timer > interval)
+            {
+                timer = 0;
 
-            //x座標のランダム生成
-            x_rnd = Random.Range(x_low, x_high);
+                enemycount++;
 
-            //y座標のランダム生成
-            y_rnd = Random.Range(y_low, y_high);
+                //x座標のランダム生成
+                x_rnd = Random.Range(x_low, x_high);
 
-            //ランダム座標を割り当て
-            transform.position = new Vector3(x_rnd, y_rnd, 0);
+                //y座標のランダム生成
+                y_rnd = Random.Range(y_low, y_high);
 
-            //エネミーの生成用ランダム
-            enemyrnd = Random.Range(0, enemyboxes.Count);
-            
-            //エネミーの生成
-            Instantiate(enemyboxes[enemyrnd], transform.position, Quaternion.identity);
+                //ランダム座標を割り当て
+                transform.position = new Vector3(x_rnd, y_rnd, 0);
+
+                //エネミーの生成用ランダム
+                //enemyrnd = Random.Range(0, enemyboxes.Count);
+
+                //エネミーの生成
+                Instantiate(enemyboxes[enemyrnd], transform.position, Quaternion.identity);
+            }
         }
+    }
+
+    void BossRespawn()
+    {
+        if (wave != old_wave)
+        {
+            Instantiate(boss, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void SetEnemyDeathCount(int count)
+    {
+        enemydeathcount += count;
+    }
+
+    public void SceneChange()
+    {
+
     }
 }
