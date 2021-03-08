3543ceb55803335cbe57cc974d7749a31124d448
uedaryota <mr.335186@gmail.com>
2021-03-08 10:28:21 +0900

    シーン遷移の追加

---
+++ iijimahan/Assets/Script/GameSceneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    private GameObject enemymanager;
+    private EnemyManager enemymanagerscript;
+    private GameObject player;
+    private PlayerControl playerscript;
+
+    void Start()
+    {
+        enemymanager = GameObject.Find("EnemyManager");
+        enemymanagerscript = enemymanager.GetComponent<EnemyManager>();
+        player = GameObject.Find("Player");
+        playerscript = player.GetComponent<PlayerControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemymanagerscript.GetGameClear() == true)
+        {
+            SceneManager.LoadScene("GameClearScene");
+        }
+        //if(playerscript.GetisDead() == true)
+        //{
+        //    SceneManager.LoadScene("GameOverScene");
+        //}
+    }
+}
