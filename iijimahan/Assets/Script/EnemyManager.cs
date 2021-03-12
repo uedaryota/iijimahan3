88459a051e5af7bfdc65d320496464be1bc61a19
uedaryota <mr.335186@gmail.com>
2021-03-12 09:50:24 +0900

    エネミーの沸き方を変更

--- iijimahan/Assets/Script/EnemyManager.cs
+++ iijimahan/Assets/Script/EnemyManager.cs
@@ -8,10 +8,9 @@ public class EnemyManager : MonoBehaviour
     [SerializeField, Header("bossオブジェクト")] Object[] bossboxes;
     [SerializeField, Header("wave毎のenemyの生成上限")] int[] enemycountlimit;
     [SerializeField, Header("wave毎の生成間隔")] float[] interval;
-    [SerializeField, Header("enemyのランダム生成用のy座標(高)")] float y_high = 0;
-    [SerializeField, Header("enemyのランダム生成用のy座標(低)")] float y_low = 0;
-    [SerializeField, Header("enemyのランダム生成用のx座標(高)")] float x_high = 0;
-    [SerializeField, Header("enemyのランダム生成用のx座標(低)")] float x_low = 0;
+    [SerializeField, Header("wave毎の沸き方(1:上下交互, 2:上2回下2回)")] int[] waverespawn;
+    [SerializeField, Header("enemyの生成用のy座標")] float y_pos = 20;
+    [SerializeField, Header("enemyの生成用のx座標")] float x_pos = 20;
     [SerializeField, Header("bossの生成用のx座標")] float x_bosspos = 0;
     [SerializeField, Header("bossの生成用のy座標")] float y_bosspos = 0;
 
@@ -25,6 +24,8 @@ public class EnemyManager : MonoBehaviour
     private float y_rnd;
     private int old_wave;
     private float EnemyChackInterval = 1;
+    private float old_pos_chack;
+    private float old_old_pos_chack;
 
     private int enemyrnd;
     private int enemycount;//エネミー生成用のカウント
@@ -34,6 +35,8 @@ public class EnemyManager : MonoBehaviour
     {
         bosschack = true;
         gameclear = false;
+        old_pos_chack = y_pos;
+        old_old_pos_chack *= -y_pos;
     }
 
     void Update()
@@ -51,14 +54,12 @@ public class EnemyManager : MonoBehaviour
         switch (wave)
         {
             case 1:
-                enemyrnd = Random.Range(0, 2);
-                EnemyRespawn();
+                EnemyRespawn(waverespawn[wave - 1]);
                 return;
 
             case 2:
-                enemyrnd = Random.Range(0, 2);
                 BossRespawn();
-                EnemyRespawn();
+                EnemyRespawn(waverespawn[wave - 1]);
                 return;
 
             case 3:
@@ -71,7 +72,7 @@ public class EnemyManager : MonoBehaviour
         }
     }
 
-    void EnemyRespawn()
+    void EnemyRespawn(int respawn)
     {
         timer += Time.deltaTime;
         
@@ -82,20 +83,40 @@ public class EnemyManager : MonoBehaviour
             if (timer >= interval[wave - 1])
             {
                 timer = 0;
-
                 enemycount++;
+                old_old_pos_chack = old_pos_chack;
+                old_pos_chack = y_pos;
+                switch (respawn)
+                {
+                    case 1:
+                        if(old_pos_chack == y_pos)
+                        {
+                            y_pos *= -1;
+                        }
+                        //座標の割り当て
+                        transform.position = new Vector3(x_pos, y_pos, 0);
+                        //エネミーの生成
+                        Instantiate(enemyboxes[enemyrnd], transform.position, Quaternion.identity);
+                        return;
 
-                //x座標のランダム生成
-                x_rnd = Random.Range(x_low, x_high);
+                    case 2:
+                        if(old_old_pos_chack == y_pos)
+                        {
+                            y_pos *= -1;
+                        }
+                        //座標の割り当て
+                        transform.position = new Vector3(x_pos, y_pos, 0);
+                        //エネミーの生成
+                        Instantiate(enemyboxes[enemyrnd], transform.position, Quaternion.identity);
+                        return;
 
-                //y座標のランダム生成
-                y_rnd = Random.Range(y_low, y_high);
-
-                //ランダム座標を割り当て
-                transform.position = new Vector3(x_rnd, y_rnd, 0);
-
-                //エネミーの生成
-                Instantiate(enemyboxes[enemyrnd], transform.position, Quaternion.identity);
+                    default:
+                        //座標の割り当て
+                        transform.position = new Vector3(x_pos, y_pos, 0);
+                        //エネミーの生成
+                        Instantiate(enemyboxes[enemyrnd], transform.position, Quaternion.identity);
+                        return;
+                }
             }
         }
         else
