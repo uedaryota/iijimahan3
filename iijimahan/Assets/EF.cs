4621d1f110208210c6432b32841aef0b47d3ea67
dukecrimson <dukecrimson1013@gmail.com>
2021-03-13 10:23:03 +0900

    着弾エフェクト前準備

---
+++ iijimahan/Assets/EF.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EF : MonoBehaviour
+{
+    MeshRenderer mr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale *= 1.02f;
+
+        mr.material.color -= new Color(0.004f, 0.004f, 0.004f, 0.004f);
+
+        if(mr.material.color.a <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
