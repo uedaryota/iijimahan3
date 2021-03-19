8e87922dd8b969e2ec70b153e4cbec4105db3328
uedaryota <mr.335186@gmail.com>
2021-03-19 14:31:51 +0900

    エネミーマネージャーを見やすく改良

--- iijimahan/Assets/Script/EnemyManager.cs
+++ iijimahan/Assets/Script/EnemyManager.cs
@@ -1,23 +1,133 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
 
 public class EnemyManager : MonoBehaviour
 {
+    private enum wave_enemy
+    {
+        Wave1の敵,
+        Wave2の敵,
+        Wave3の敵,
+        Wave4の敵,
+        Wave5の敵,
+        Wave6の敵,
+        Wave7の敵,
+        Wave8の敵,
+        Wave9の敵,
+        Wave10の敵,
+    }
+
+    private enum enemy_box
+    {
+        敵1体目,
+        敵2体目,
+        敵3体目,
+        敵4体目,
+        敵5体目,
+        敵6体目,
+        敵7体目,
+        敵8体目,
+        敵9体目,
+        敵10体目,
+        敵11体目,
+        敵12体目,
+        敵13体目,
+        敵14体目,
+        敵15体目,
+        敵16体目,
+        敵17体目,
+        敵18体目,
+        敵19体目,
+        敵20体目,
+        敵31体目,
+        敵32体目,
+        敵33体目,
+        敵34体目,
+        敵35体目,
+        敵36体目,
+        敵37体目,
+        敵38体目,
+        敵39体目,
+        敵41体目,
+        敵42体目,
+        敵43体目,
+        敵44体目,
+        敵45体目,
+        敵46体目,
+        敵47体目,
+        敵48体目,
+        敵49体目,
+        敵51体目,
+        敵52体目,
+        敵53体目,
+        敵54体目,
+        敵55体目,
+        敵56体目,
+        敵57体目,
+        敵58体目,
+        敵59体目,
+        敵61体目,
+        敵62体目,
+        敵63体目,
+        敵64体目,
+        敵65体目,
+        敵66体目,
+        敵67体目,
+        敵68体目,
+        敵69体目,
+        敵71体目,
+        敵72体目,
+        敵73体目,
+        敵74体目,
+        敵75体目,
+        敵76体目,
+        敵77体目,
+        敵78体目,
+        敵79体目,
+        敵81体目,
+        敵82体目,
+        敵83体目,
+        敵84体目,
+        敵85体目,
+        敵86体目,
+        敵87体目,
+        敵88体目,
+        敵89体目,
+        敵91体目,
+        敵92体目,
+        敵93体目,
+        敵94体目,
+        敵95体目,
+        敵96体目,
+        敵97体目,
+        敵98体目,
+        敵99体目,
+    }
+    
     [System.Serializable]
     public struct WaveEnemy
     {
         [SerializeField, Header("生成するエネミーオブジェクト")]
+        [EnumLabel(typeof(enemy_box))]
         public Object[] enemyboxes_Manual;
         [SerializeField, Header("生成数")]
         public int enemycountlimit_Manual;
         [SerializeField, Header("生成位置")]
+        [EnumLabel(typeof(enemy_box))]
         public Vector2[] pos_Manual;
         [SerializeField, Header("生成間隔")]
+        [EnumLabel(typeof(enemy_box))]
         public float[] interval_Manual;
     }
 
-    [SerializeField, Header("wave毎の手入力エネミー")] WaveEnemy[] enemy_Manual;
+    [SerializeField, Header("wave毎の手入力エネミー")]
+    [EnumLabel(typeof(wave_enemy))]
+    public WaveEnemy[] enemy_Manual;
+        
 
     [SerializeField, Header("enemyboxオブジェクト")] Object[] enemyboxes;
     [SerializeField, Header("bossオブジェクト")] Object[] bossboxes;
