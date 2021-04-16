26ffdf00af6819d493f9b14c0fb1b49515016a39
sugahararyota <s.ryota072@gmail.com>
2021-04-17 06:32:22 +0900

    ゲージエネルギーの回収方法を変更

--- iijimahan/Assets/Script/GaugeEnergyControl.cs
+++ iijimahan/Assets/Script/GaugeEnergyControl.cs
@@ -7,7 +7,9 @@ public class GaugeEnergyControl : MonoBehaviour
     private float easingCount = 0;
     //private bool easingFlag = false;
     public int num = 80;
+    private int num2 = 1200;
     public float maxSpeed = 30;
+    private float maxSpeed2 = 15;
     private float speed = 0.0f;
     private Vector3 playerPos;
     private Vector3 velocity;
@@ -15,11 +17,17 @@ public class GaugeEnergyControl : MonoBehaviour
     private GameObject player;
     private GameObject gauge;
 
+    private GameObject target;
+
+    private bool hit = false;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         gauge = GameObject.FindGameObjectWithTag("GaugeTarget");
+        target = GameObject.FindGameObjectWithTag("GaugeTarget2");
+        playerPos = target.transform.position;
     }
 
     // Update is called once per frame
@@ -29,9 +37,16 @@ public class GaugeEnergyControl : MonoBehaviour
         if (Time.timeScale <= 0) return;
 
         //playerPos = player.transform.position;
-        playerPos = gauge.transform.position;
+        //playerPos = gauge.transform.position;
 
-        speed = Easing.SineInOut(easingCount, num, speed, maxSpeed);
+        if (!hit) speed = Easing.SineInOut(easingCount, num, speed, maxSpeed);
+        else
+        {
+            speed = Easing.SineInOut(easingCount, num2, speed, maxSpeed2);
+            playerPos = gauge.transform.position;
+        }
+
+
         velocity = Move();
         transform.position += velocity * speed * Time.deltaTime * 2;
 
@@ -65,12 +80,31 @@ public class GaugeEnergyControl : MonoBehaviour
             player.GetComponent<PlayerControl>().GaugeUp();
             Destroy(this.gameObject);
         }
+
+        if (other.gameObject.tag == "GaugeTarget2" && hit == false)
+        {
+            playerPos = gauge.transform.position;
+            easingCount = 0;
+            speed = 0;
+            hit = true;
+        }
     }
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "GaugeTarget")
         {
+            player.GetComponent<PlayerControl>().GaugeUp();
+            Destroy(this.gameObject);
+        }
+
+        if (col.gameObject.tag == "GaugeTarget2" && hit == false)
+        {
+            //playerPos = gauge.transform.position;
+            //easingCount = 0;
+            //speed = 0;
+            //hit = true;
+
             player.GetComponent<PlayerControl>().GaugeUp();
             Destroy(this.gameObject);
         }
--- iijimahan/Assets/Script/Player/GaugeTargetScript.cs
+++ iijimahan/Assets/Script/Player/GaugeTargetScript.cs
@@ -7,6 +7,8 @@ public class GaugeTargetScript : MonoBehaviour
 {
     [SerializeField]
     public Image GreenGauge;
+
+    public GameObject gaugeEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +21,16 @@ public class GaugeTargetScript : MonoBehaviour
         float pos = 0;
         pos = GreenGauge.fillAmount * 8f - 5;
         transform.position = new Vector3(transform.position.x, pos, transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "GaugeEnergy")
+        {
+            GameObject effect = Instantiate(gaugeEffect);
+            effect.transform.position = transform.position + new Vector3(0,-1,0);
+            effect.transform.localScale = new Vector3(2, 6, 2);
+            //effect.GetComponent<PlayerBurstScript>().SetModeFlag(true);
+        }
     }
 }
