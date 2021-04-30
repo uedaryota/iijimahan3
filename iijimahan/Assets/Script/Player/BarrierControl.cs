ef04ac99d75bc2cf0dda67e5ffc290fdb28c06cf
sugahararyota <s.ryota072@gmail.com>
2021-04-30 14:13:31 +0900

    プレイヤーの弾が当たった時に出るバリアの消え方変更（フェード）

--- iijimahan/Assets/Script/Player/BarrierControl.cs
+++ iijimahan/Assets/Script/Player/BarrierControl.cs
@@ -16,16 +16,25 @@ public class BarrierControl : MonoBehaviour
 
     private GameObject boss;
 
+    public SpriteRenderer sr;
+
     void Start()
     {
         mate.color = new Color(1, 1, 0, 1);
-       boss = GameObject.FindGameObjectWithTag("Boss");
+        boss = GameObject.FindGameObjectWithTag("Boss");
     }
 
     // Update is called once per frame
     void Update()
     {
         counter += Time.deltaTime * 60;
+
+        if (counter > 20)
+        {
+            alpha += Time.deltaTime * 2f;
+            sr.color = sr.color - new Color(0, 0, 0, alpha);
+        }
+
         if (counter > 30)
         {
             Destroy(this.gameObject);
