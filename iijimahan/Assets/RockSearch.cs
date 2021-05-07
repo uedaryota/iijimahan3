4a44f3b719e4ea63f6da55bb39c55db59eb762bf
LAPTOP-CT2SJ4KH\machi <machida.1331@gmail.com>
2021-05-07 11:19:42 +0900

    隕石から離れるＡＩ

---
+++ iijimahan/Assets/RockSearch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSearch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Rock")
+        {
+           
+            this.transform.parent.GetComponent<EnemyHitMove>().SetRock(other.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- iijimahan/Assets/Script/EnemyHitMove.cs
+++ iijimahan/Assets/Script/EnemyHitMove.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class EnemyHitMove : MonoBehaviour
 {
+    GameObject rock;
+    Vector3 velocity = 0;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+   
     private void OnTriggerStay(Collider other)
     {
         if (other.transform.tag == "Enemy" || other.transform.tag == "Player"|| other.transform.tag == "Friend")  
@@ -18,16 +20,36 @@ public class EnemyHitMove : MonoBehaviour
             vel = vel.normalized/ 30 ;
             transform.position -= vel ;
         }
-        if ( other.transform.tag == "Rock")
+        if (other.transform.tag == "Rock")
         {
             Vector3 vel = other.transform.position - transform.position;
             vel = vel.normalized / 3;
             transform.position -= vel;
         }
     }
+    void AvoidRock()
+    {
+        float speed = 3;
+        float targetDistance = 7;
+        if (rock != null)
+        {
+            if (Vector3.Distance(rock.transform.position, transform.position) <= targetDistance)
+            {
+              //  Debug.Log("aaa");
+                velocity += Vector3.Normalize(rock.transform.position - transform.position);
+                velocity = Vector3.Normalize(velocity) * speed;
+                transform.position -= velocity * Time.deltaTime;
+            }
+
+        }
+    }
+    public void SetRock(GameObject _rock)
+    {
+        rock = _rock;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        AvoidRock();
     }
 }
