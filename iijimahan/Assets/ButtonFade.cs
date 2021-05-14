b20ba4c4f456aa01feb0871d1bd5125d39dc5c61
LAPTOP-CT2SJ4KH\machi <machida.1331@gmail.com>
2021-05-14 10:26:56 +0900

    タイトルシーン演出

---
+++ iijimahan/Assets/ButtonFade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFade : MonoBehaviour
+{
+    Text text;
+    Button button;
+    float alpha = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponentInChildren<Text>();
+        button = GetComponent<Button>();
+        alpha = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alpha -= Time.deltaTime*1.5f;
+        if(alpha<0)
+        {
+            alpha = 0;
+        }
+        text.color = new Color(text.color.r, text.color.g, text.color.b, (3 - alpha) / 3);
+        button.GetComponentInChildren<Image>().color = new Color
+            (button.GetComponentInChildren<Image>().color.r,
+            button.GetComponentInChildren<Image>().color.g,
+            button.GetComponentInChildren<Image>().color.b,
+          (  3 - alpha) / 3);
+    }
+}
