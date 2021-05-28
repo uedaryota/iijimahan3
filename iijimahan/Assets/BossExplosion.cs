2a8100bc168491406f087e5221711c66d193b9a0
uedaryota <mr.335186@gmail.com>
2021-05-28 16:13:13 +0900

    各ボスに爆発SE追加

--- iijimahan/Assets/BossExplosion.cs
+++ iijimahan/Assets/BossExplosion.cs
@@ -12,10 +12,17 @@ public class BossExplosion : MonoBehaviour
     [SerializeField, Header("爆発サイズ(大きい)")]private Vector3 effectScaleBig = new Vector3(2.5f, 2.5f, 2.5f);
     [SerializeField, Header("ボスの爆発中の速さ")]private Vector3 speed = new Vector3(-0.1f, -0.1f, 0.0f);
     [SerializeField, Header("ボスの完全爆発までの時間")] private int time = 3;
+    [SerializeField, Header("爆発SE")] private AudioClip ExplosionSE;
+    private GameObject option;
+    private Option script;
+    private AudioSource audiosource;
 
 
     void Start()
     {
+        audiosource = GetComponent<AudioSource>();
+        option = GameObject.Find("Option");
+        script = option.GetComponent<Option>();
         hp = gameObject.GetComponent<BossHp>();
         EffectFlag = false;
     }
@@ -37,26 +44,31 @@ public class BossExplosion : MonoBehaviour
 
     IEnumerator ExplosionEffect()
     {
+        audiosource.volume = script.GetSEVolume();
         for (int i = 0; i < time; i++)
         {
             GameObject effect = Instantiate(Explosion);
             effect.transform.position = transform.position + effectpos[0];
             effect.transform.localScale = effectScaleSmall;
+            audiosource.PlayOneShot(ExplosionSE);
             yield return new WaitForSeconds(0.5f);
 
             GameObject effect2 = Instantiate(Explosion);
             effect2.transform.position = transform.position + effectpos[1];
             effect2.transform.localScale = effectScaleSmall;
+            audiosource.PlayOneShot(ExplosionSE);
             yield return new WaitForSeconds(0.5f);
 
             GameObject effect3 = Instantiate(Explosion);
             effect3.transform.position = transform.position + effectpos[2];
             effect3.transform.localScale = effectScaleSmall;
+            audiosource.PlayOneShot(ExplosionSE);
             yield return new WaitForSeconds(0.5f);
 
             GameObject effect4 = Instantiate(Explosion);
             effect4.transform.position = transform.position + effectpos[3];
             effect4.transform.localScale = effectScaleBig;
+            audiosource.PlayOneShot(ExplosionSE);
             yield return new WaitForSeconds(0.1f);
             Debug.Log(i + "回目");
         }
